211d25e9067c1aa7277387363612f9cfdc2f7b6b
goodbye <jmnyarega@gmail.com>
2022-03-13 02:38:25 +0300

    *

--- st/.local/src/st/config.h
+++ st/.local/src/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font =  "DroidSansMono Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font =  "OperatorMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=14:antialias=true:autohint=true" };
 static int borderpx = 2;
 
