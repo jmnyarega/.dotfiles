9eee20ca250c035df82a553daaafab35356d2935
jmnyarega <josiah@nairocket.com>
2024-01-22 09:25:22 +0300

    Add colors and fonts

--- dmenu/.local/src/dmenu/config.h
+++ dmenu/.local/src/dmenu/config.h
@@ -4,17 +4,17 @@
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:size=10:antialias=true:autohint=true",
+	"Mononoki Nerd Font:size=10:antialias=true:autohint=true",
 	"JoyPixels:pixelsize=10:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe1;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-    [SchemeNorm] = { "#F8F8F2", "#282A36" },
-    [SchemeSel] = { "#F8F8F2", "#6272A4"},
-    [SchemeOut] = { "#000000", "#00ffff" },
+  /*     fg         bg       */
+    [SchemeNorm] = { "#abb2bf", "#282c34" },
+    [SchemeSel] = { "#e06c75", "#282c34"},
+    [SchemeOut] = { "#e06c75", "#282c34" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
