72a584563cafaf2b4a08fe923decbf42ef187609
Josiah <josiah@scorpio.local>
2025-07-28 15:49:07 +0300

    update]

--- nvim/.config/nvim/lua/theprimeagen/lazy/local.lua
+++ nvim/.config/nvim/lua/theprimeagen/lazy/local.lua
@@ -1,17 +1,17 @@
 local local_plugins = {
-    {
-        "rfceez",
-        dir = "~/personal/rfceez",
-        config = function()
-            local rfc = require("rfceez")
-            rfc.setup()
-            vim.keymap.set("n", "<leader>ra", function() rfc.add() end)
-            vim.keymap.set("n", "<leader>rd", function() rfc.rm() end)
-            vim.keymap.set("n", "<leader>rs", function() rfc.show_notes() end)
-            vim.keymap.set("n", "[r", function() rfc.nav_next() end)
-            vim.keymap.set("n", "[[r", function() rfc.show_next() end)
-        end
-    },
+ --    {
+ --        "rfceez",
+ --         dir = "~/personal/rfceez",
+ --         config = function()
+ --             local rfc = require("rfceez")
+ --           rfc.setup()
+ --           vim.keymap.set("n", "<leader>ra", function() rfc.add() end)
+  --         vim.keymap.set("n", "<leader>rd", function() rfc.rm() end)
+  --         vim.keymap.set("n", "<leader>rs", function() rfc.show_notes() end)
+  --         vim.keymap.set("n", "[r", function() rfc.nav_next() end)
+  --          vim.keymap.set("n", "[[r", function() rfc.show_next() end)
+   --     end
+   -- },
     -- {
     --     "harpoon",
     --     dir = "~/personal/harpoon",
@@ -34,23 +34,22 @@ local local_plugins = {
     -- vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
     -- end
     -- },
-    {
-        "vim-apm",
-        dir = "~/personal/vim-apm",
-        config = function()
+   -- {
+  --      "vim-apm",
+   --     dir = "~/personal/vim-apm",
+    --    config = function()
             --[[
             local apm = require("vim-apm")
 
             apm:setup({})
             vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
             --]]
-        end
-    },
-    {
-        "vim-with-me",
-        dir = "~/personal/vim-with-me",
-        config = function() end
-    },
+   --     end
+  --  },
+   -- {
+   --     "vim-with-me",
+   --     dir = "~/personal/vim-with-me",
+    --    config = function() end
 }
 
 return local_plugins
